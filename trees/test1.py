be85565b178a4135257d78fca6ca7c5a78e591df
vamsikrishna <vamsikrishna1845644@gmail.com>
2025-10-05 14:56:17 +0530

    i think i changed something

---
+++ trees/test1.py
@@ -0,0 +1,22 @@
+print("hello")
+#why are u here\
+# is the keyboard working fine
+# indeed its is working fine 
+# is it better than the laptop keybord 
+# yes i think so because i can easily type faster on this keyboard
+# what else do u think abt this keyboard , i think i can easily type faster in this keyboars it has red switches
+#ntg
+# this is just a tyiping test , dont take anything seriously as this is just for purposes only 
+# i hope u guys dont mind me typing likes this as i am getting better anjd better with each keystroke nthe keys dance across the keyboard just like that\
+# thisdosnt mak any sense
+# not evrythig does not wevrythig has to
+for i in range(100):
+    print("nothiung")
+
+arr = []
+i = 12
+for j in range(1000):
+    arr.append(i+1) # this is a dummy function thats it
+# print the final function here 
+print(arr) # printing the final array here 
+# these are dummy this dont take anything serously
