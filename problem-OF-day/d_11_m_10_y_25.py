d536f1d2b0e74efe138298c639a7de15695b38cc
vamsikrishna <vamsikrishna1845644@gmail.com>
2025-10-11 02:36:55 +0530

    23

---
+++ problem-OF-day/d_11_m_10_y_25.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def maximumEnergy(self, energy: List[int], k: int) -> int:
+        pointer1 = 0
+        pointer2 = k
+        n = len(energy)
+        ans = float("-inf")
+        for pointer1 in range(n):
+            max_energy = 0
+            pointer2 = pointer1
+            while pointer2<n:
+                max_energy+=energy[pointer2]
+                pointer2+=k
+            ans = max(ans,max_energy)
+        return ans
+    
+sol = Solution()
+energy = [5,-10,4,3,5,-9,9,-7]
+k = 2
+print(sol.maximumEnergy(energy,k))
